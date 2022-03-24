d96a7b33e89e256bd9c450c2fbbdb0b25ba093dc
Kyle Goodwin <kylegoodwin@users.noreply.github.com>
2022-03-23 22:00:49 -0400

    Init repo, create basic type and fetch data

---
+++ types/site.ts
@@ -0,0 +1,10 @@
+export default interface Site{
+    name: String,
+    intersection: String,
+    coordinates: String,
+    hours: String,
+    weekdays: String,
+    timeOfYear: String,
+    link: String
+
+}
